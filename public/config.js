778557f0fdbfbce7bffa8746da2b639e49a349d8
Tracy_chocolatte <circejiu@gmail.com>
2026-09-19 23:59:05 -0500

    Enable Canvas bridge on production site domain

--- public/config.js
+++ public/config.js
@@ -1,3 +1,3 @@
-window.ILLINI_API_BASE = location.hostname === 'chocolatte-tracy.github.io'
+window.ILLINI_API_BASE = ['chocolatte-tracy.github.io', 'illini-day-planner.xdfvvb7ymf.chatgpt.site'].includes(location.hostname)
   ? 'https://illini-day-planner.xdfvvb7ymf.chatgpt.site'
   : '';
